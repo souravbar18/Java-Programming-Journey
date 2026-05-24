bdf0d2927ab48b6dbf4952f657a6a5616cb84614
Sourav Bar <souravbar432@gmail.com>
2026-05-24 16:53:02 +0600

    Update EvenOdd.java
    edit : First it was just for Even Odd checker but now you can check it is even Negative or Positive together.
    Thank You.

--- Basic-Programs/EvenOdd.java
+++ Basic-Programs/EvenOdd.java
@@ -1,20 +1,34 @@
+//edit version
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // Create a Scanner to take input
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter a number to check: ");
-        int num = input.nextInt();
-
-        // Logic : if a number can devide by 2 , it's a Even number
-        if (num % 2 == 0) {
-            System.out.println(num + " is an Even number.");
-        } else {
-            System.out.println(num + " is an Odd number.");
+        System.out.println("Even Odd Checker & Positive Negative Checker");
+        System.out.println("Type 'stop' to stop program");
+        while (true) {
+            System.out.print("Enter Your Number : ");
+            String number = input.next();
+            if (number.equalsIgnoreCase("stop") ) {
+                System.out.println("Thank You ! Have a great day.");
+                break;
+            }
+            try {
+                int num = Integer.parseInt(number);
+                if (num >= 0 & num % 2 == 0) {
+                    System.out.println(num + " : It is a Positive and Even Number.");
+                } else if (num >= 0 & num % 2 != 0) {
+                    System.out.println(num + " : It is a Positive number but not a Even number");
+                } else if (num < 0 & num % 2 == 0) {
+                    System.out.println(num + " : It is a Negative and Even number");
+                } else {
+                    System.out.println(num + " : It is a Negative number but not a Even number");
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println(number + " : It is not a number , please input a number !");
+                continue;
+            }
         }
-
         input.close();
     }
 }
